5b1f950beab4ab7b6aa77e73fed3b814fa731356
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-04 14:41:01 +0700

    feat: unbounded knapsack topDownMemoized submit

--- 7-unbounded_knapsack/submit-1.ts
+++ 7-unbounded_knapsack/submit-1.ts
@@ -31,4 +31,44 @@ function topDown(
   return max.current;
 }
 
-export { topDown };
+function topDownMemoized(profits, weights, capacity) {
+  const cache = {};
+
+  function recursiveFn(capacity, curIdx, curProfit) {
+    const [cap, idx] = Array.from(arguments);
+
+    if (String([cap, idx]) in cache) return cache[String([cap, idx])];
+
+    // base case
+    if (curIdx === profits.length) {
+      // globalMax = Math.max(globalMax, curProfit);
+      return curProfit;
+    }
+
+    // iterate items, for each: maxAmount can take ? n, cases are n to 0;
+    const profit = profits[curIdx];
+    const weight = weights[curIdx];
+
+    const maxAmount = ~~(capacity / weight);
+
+    let maxProfit = 0;
+
+    for (let j = maxAmount; j >= 0; j--) {
+      const result = recursiveFn(
+        capacity - j * weight,
+        curIdx + 1,
+        curProfit + j * profit
+      );
+
+      maxProfit = Math.max(result, maxProfit);
+    }
+
+    cache[String([cap, idx])] = maxProfit;
+
+    return maxProfit;
+  }
+
+  return recursiveFn(capacity, 0, 0);
+}
+
+export { topDown, topDownMemoized };
--- 7-unbounded_knapsack/test.ts
+++ 7-unbounded_knapsack/test.ts
@@ -1,10 +1,19 @@
-import { topDown } from "./submit-1";
+import { topDown, topDownMemoized } from "./submit-1";
 
 test("topDown", () => {
   const weights = [1, 2, 3];
   const profits = [15, 20, 50];
   const capacity = 5;
   const result = topDown(profits, weights, capacity);
+
+  expect(result).toBe(80);
+});
+
+test("topDownMemoized", () => {
+  const weights = [1, 2, 3];
+  const profits = [15, 20, 50];
+  const capacity = 5;
+  const result = topDownMemoized(profits, weights, capacity);
 
   expect(result).toBe(80);
 });
