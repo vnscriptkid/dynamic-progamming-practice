dddadf1f34945f5486cfe0535652da1fca630110
Thanh Nguyen <vnscriptkid@gmail.com>
2022-02-25 14:10:23 +0700

    feat: rod-cutting top-down-cache submit 1

--- 2-unbounded_knapsack/2-rod-cutting/submit-1.test.ts
+++ 2-unbounded_knapsack/2-rod-cutting/submit-1.test.ts
@@ -1,8 +1,15 @@
-import { naive } from "./submit-1";
+import { naive, topDownCache } from "./submit-1";
 
 test("naive", () => {
   const lengths = [1, 2, 3, 4, 5];
   const prices = [2, 6, 7, 10, 13];
   const result = naive(lengths, prices, 5);
+  expect(result).toEqual(14);
+});
+
+test("topDownCache", () => {
+  const lengths = [1, 2, 3, 4, 5];
+  const prices = [2, 6, 7, 10, 13];
+  const result = topDownCache(lengths, prices, 5);
   expect(result).toEqual(14);
 });
--- 2-unbounded_knapsack/2-rod-cutting/submit-1.ts
+++ 2-unbounded_knapsack/2-rod-cutting/submit-1.ts
@@ -28,4 +28,41 @@ function naive(
   return Math.max(p1, p2);
 }
 
-export { naive };
+function topDownCache(
+  lengths: number[],
+  prices: number[],
+  total: number,
+  curIdx = 0,
+  curPrice = 0,
+  cache: Record<string, number> = {}
+): number {
+  const cacheKey = String([total, curIdx, curPrice]);
+
+  if (!(cacheKey in cache)) {
+    if (curIdx === prices.length || total <= 0) {
+      return curPrice;
+    }
+
+    // choices
+    // take cur length
+    let p1 = curPrice;
+    if (total >= lengths[curIdx]) {
+      p1 = naive(
+        lengths,
+        prices,
+        total - lengths[curIdx],
+        curIdx,
+        curPrice + prices[curIdx]
+      );
+    }
+
+    // skip cur length
+    const p2 = naive(lengths, prices, total, curIdx + 1, curPrice);
+
+    cache[cacheKey] = Math.max(p1, p2);
+  }
+
+  return cache[cacheKey];
+}
+
+export { naive, topDownCache };
